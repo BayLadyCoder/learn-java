40905918f103e82cd7298d34c0b022a68d645079
BayLadyCoder <rachada_32@hotmail.com>
2024-03-24 20:56:19 -0400

    Arrays

--- src/com/bay/Main.java
+++ src/com/bay/Main.java
@@ -1,6 +1,7 @@
 package com.bay;
 
 import java.awt.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -116,5 +117,40 @@ public class Main {
         System.out.println("pointA = " + pointA);
         pointA.move(11,12);
         System.out.println("pointA = " + pointA);
+
+        // Arrays
+        int[] numbers =new int[3];
+        numbers[0] = 1;
+        numbers[1] = 33;
+        numbers[2] = 6;
+        System.out.println(Arrays.toString(numbers)); // [1, 33, 6]
+        System.out.println(numbers.length); // 3
+
+
+        int[] numbers2 = {11, 4, 32, 56, 77};
+        System.out.println(Arrays.toString(numbers2)); // [11, 4, 32, 56, 77]
+        System.out.println(numbers2.length); // 5
+
+        String[] names = {"Bay", "Sam", "Ringo"};
+        System.out.println(Arrays.toString(names));
+
+
+        // Default values in Arrays
+        int[] nums1 = new int[3]; // default value of int array is 0
+        System.out.println(Arrays.toString(nums1)); // [0, 0, 0]
+        nums1[1] = 23;
+        System.out.println(Arrays.toString(nums1)); // [0, 23, 0]
+        int[] nums2 = new int[3];
+        Arrays.fill(nums2, 15); // override default values
+        System.out.println(Arrays.toString(nums2)); // [15, 15, 15]
+
+        boolean[] booleanArr = new boolean[3]; // default value of boolean array is false
+        System.out.println(Arrays.toString(booleanArr)); // [false, false, false]
+
+        double[] doubleArr = new double[3]; // default value of double array is 0.0
+        System.out.println(Arrays.toString(doubleArr)); // [0.0, 0.0, 0.0]
+
+        String[] stringArr = new String[3]; // default value of String array is null
+        System.out.println(Arrays.toString(stringArr)); // [null, null, null]
     }
 }

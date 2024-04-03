7621ec69f63afd2eccd25d96c1248e9260c7663d
BayLadyCoder <rachada_32@hotmail.com>
2024-04-03 19:49:56 -0400

    If else

---
+++ src/IfElse.java
@@ -0,0 +1,10 @@
+public class IfElse {
+    public static void main (String args[]){
+        String weather = "sunny";
+        if(weather.equals("raining")){ // or if(weather == "raining") {
+            System.out.println("I am staying home.");
+        } else {
+            System.out.println("I'm going out for a walk.");
+        }
+    }
+}

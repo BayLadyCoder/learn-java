d0a89f9e75eabf8d89429a5c2e1fa0ca6599982f
BayLadyCoder <rachada_32@hotmail.com>
2024-04-05 19:35:48 -0400

    Int method

---
+++ src/IntMethod.java
@@ -0,0 +1,11 @@
+public class IntMethod {
+    public static void main(String args[]) {
+        System.out.println(add(1, 2));
+        System.out.println(add(19, 22));
+        System.out.println(add(35, 988));
+    }
+
+    public static int add(int num1, int num2) {
+        return num1 + num2;
+    }
+}
